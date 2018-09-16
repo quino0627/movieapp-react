76c5fbc23ab1d88c58270c6041beb2a060eddc8a
송동욱 <quino0627@korea.ac.kr>
2018-09-16 21:41:27 +0900

    async await

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -15,16 +15,33 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating");
+    this._getMovies();
     //console.log(fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating"));
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />;
     });
+    console.log(movies)
     return movies;
   };
+
+  _getMovies = async () => {
+    const movies = await this._callApi() //이게 성공적으로 이루어지고 나면 ~ 
+    this.setState({
+      movies //moderns
+      //moves : movies 
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(potato => potato.json())
+    .then(json => json.data.movies) //화살표 표시는 리턴작성할필요없음 모던js라서 자동임
+    .catch(err => console.log(err))
+  }
+
   render() {
     // console.log("Did render");
 
